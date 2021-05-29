bd56932adde40e4bc95baf5c177e412dceb9af1d
ishii1648 <ishiisho0509@gmail.com>
2021-05-29 20:51:50 +0900

    init create
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ishii1648/cloud-run-sdk
+
+go 1.15
+
+require (
+	cloud.google.com/go v0.82.0
+	github.com/rs/zerolog v1.22.0
+	google.golang.org/api v0.47.0
+	google.golang.org/grpc v1.38.0
+)
---
+++ http.go
@@ -0,0 +1,72 @@
+package sdk
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+type IndexHandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+func (fn IndexHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fn(w, r)
+}
+
+type Adapter func(http.Handler) http.Handler
+
+func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
+	for _, adapter := range adapters {
+		h = adapter(h)
+	}
+	return h
+}
+
+func InjectLogger(logger zerolog.Logger) Adapter {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if isCloudRun() {
+				logger = logger.With().Timestamp().Logger().Hook(sourceLocationHook)
+				r = r.WithContext(logger.WithContext(r.Context()))
+
+				traceID, _ := traceContextFromHeader(r.Header.Get("X-Cloud-Trace-Context"))
+				if traceID == "" {
+					h.ServeHTTP(w, r)
+					return
+				}
+				trace := fmt.Sprintf("projects/%s/traces/%s", ProjectID, traceID)
+
+				logger.UpdateContext(func(c zerolog.Context) zerolog.Context {
+					return c.Str("logging.googleapis.com/trace", trace)
+				})
+			} else {
+				logger = logger.With().Timestamp().Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
+				r = r.WithContext(logger.WithContext(r.Context()))
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+func RegisterDefaultHTTPServer(fn IndexHandlerFunc) *http.Server {
+	port := "8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	}
+
+	hostAddr := "0.0.0.0"
+	if h := os.Getenv("HOST_ADDR"); h != "" {
+		hostAddr = h
+	}
+
+	logger := zerolog.New(os.Stdout)
+	mux := http.NewServeMux()
+	mux.Handle("/", Adapt(fn, InjectLogger(logger)))
+
+	return &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", hostAddr, port),
+		Handler: mux,
+	}
+}

f2fef57505812f556ff9f1db50bdfb2a483d70a8
ishii1648 <ishiisho0509@gmail.com>
2021-06-27 15:47:51 +0900

    change logging directory
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- logging/zerolog/log.go
+++ logging/zerolog/log.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/ishii1648/cloud-run-sdk/util"
 	"github.com/rs/zerolog"
 )
 
@@ -12,15 +13,20 @@ type Logger struct {
 	logger *zerolog.Logger
 }
 
-func SetLogger(cloudrun bool) zerolog.Logger {
+func SetLogger(debug bool) zerolog.Logger {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
 	logger := zerolog.New(os.Stdout)
 
-	if cloudrun {
+	if util.IsCloudRun() {
 		zerolog.TimeFieldFormat = time.RFC3339Nano
 		zerolog.LevelFieldName = "severity"
 		zerolog.LevelFieldMarshalFunc = LevelFieldMarshalFunc
 
-		return logger.With().Timestamp().Logger().Hook(&callerHook{})
+		return logger.With().Timestamp().Logger().Hook(&CallerHook{})
 	}
 
 	return logger.With().Timestamp().Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
@@ -29,6 +35,21 @@ func SetLogger(cloudrun bool) zerolog.Logger {
 func NewLogger(logger *zerolog.Logger) Logger {
 	return Logger{
 		logger: logger,
+	}
+}
+
+func LevelFieldMarshalFunc(l zerolog.Level) string {
+	// mapping to Cloud Logging LogSeverity
+	// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+	switch l {
+	case zerolog.DebugLevel:
+		return "DEBUG"
+	case zerolog.InfoLevel:
+		return "INFO"
+	case zerolog.ErrorLevel:
+		return "ERROR"
+	default:
+		return "INFO"
 	}
 }
 
@@ -46,14 +67,6 @@ func (l Logger) Info(args ...interface{}) {
 
 func (l Logger) Infof(format string, args ...interface{}) {
 	l.logger.Info().Msgf(format, args...)
-}
-
-func (l Logger) Warn(args ...interface{}) {
-	l.logger.Warn().Msg(fmt.Sprint(args...))
-}
-
-func (l Logger) Warnf(format string, args ...interface{}) {
-	l.logger.Warn().Msgf(format, args...)
 }
 
 func (l Logger) Error(args ...interface{}) {
---
+++ logging/zerolog/trace.go
@@ -0,0 +1,33 @@
+package zerolog
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var (
+	// For trace header, see https://cloud.google.com/trace/docs/troubleshooting#force-trace
+	traceHeaderRegExp = regexp.MustCompile(`^\s*([0-9a-fA-F]+)(?:/(\d+))?(?:;o=[01])?\s*$`)
+)
+
+func TraceContextFromHeader(header string) (string, string) {
+	matched := traceHeaderRegExp.FindStringSubmatch(header)
+	if len(matched) < 3 {
+		return "", ""
+	}
+
+	traceID, spanID := matched[1], matched[2]
+	if spanID == "" {
+		return traceID, ""
+	}
+	spanIDInt, err := strconv.ParseUint(spanID, 10, 64)
+	if err != nil {
+		// invalid
+		return "", ""
+	}
+	// spanId for cloud logging must be 16-character hexadecimal number.
+	// See: https://cloud.google.com/trace/docs/trace-log-integration#associating
+	spanIDHex := fmt.Sprintf("%016x", spanIDInt)
+	return traceID, spanIDHex
+}

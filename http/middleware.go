308b921fea179a392bc3a0e9f563dadd92aaa82d
ishii1648 <ishiisho0509@gmail.com>
2021-06-28 23:02:50 +0900

    change inject logger
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- http/middleware.go
+++ http/middleware.go
@@ -22,16 +22,17 @@ func Chain(h http.Handler, middlewares ...Middleware) http.Handler {
 func InjectLogger(projectID string, isCloudRun, debug bool) Middleware {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger := clog.SetLogger(os.Stdout, debug, util.IsCloudRun())
+			logger := clog.SetLogger(os.Stdout, debug, util.IsCloudRun(), true)
 
-			if isCloudRun {
-				traceID, _ := util.TraceContextFromHeader(r.Header.Get("X-Cloud-Trace-Context"))
+			if !isCloudRun {
+				h.ServeHTTP(w, r.WithContext(logger.WithContext(r.Context())))
+				return
+			}
 
-				if traceID != "" {
-					logger.UpdateContext(func(c zerolog.Context) zerolog.Context {
-						return c.Str("logging.googleapis.com/trace", fmt.Sprintf("projects/%s/traces/%s", projectID, traceID))
-					})
-				}
+			if traceID := util.GetTraceIDFromHeader(r.Header.Get("X-Cloud-Trace-Context")); traceID != "" {
+				logger.UpdateContext(func(c zerolog.Context) zerolog.Context {
+					return c.Str("logging.googleapis.com/trace", fmt.Sprintf("projects/%s/traces/%s", projectID, traceID))
+				})
 			}
 
 			h.ServeHTTP(w, r.WithContext(logger.WithContext(r.Context())))

ebfee16037dd92bef7cf33f044cc20d989445366
ishii1648 <ishiisho0509@gmail.com>
2021-07-23 20:52:44 +0900

    change grpc api
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- grpc/interceptor.go
+++ grpc/interceptor.go
@@ -3,56 +3,39 @@ package grpc
 import (
 	"context"
 	"fmt"
-	"os"
 
-	clog "github.com/ishii1648/cloud-run-sdk/logging/zerolog"
+	"github.com/ishii1648/cloud-run-sdk/logging/zerolog"
 	"github.com/ishii1648/cloud-run-sdk/util"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	pkgzerolog "github.com/rs/zerolog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
-func LoggerInterceptor(projectID string, isCloudRun, debug bool) grpc.UnaryServerInterceptor {
+func LoggerInterceptor(l *zerolog.Logger, projectID string) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		logger := clog.SetLogger(os.Stdout, debug, true)
-		ctx = logger.WithContext(ctx)
+		l.ZeroLogger.UpdateContext(func(c pkgzerolog.Context) pkgzerolog.Context {
+			return c.Str("method", info.FullMethod)
+		})
 
-		if !isCloudRun {
-			return handler(ctx, req)
+		if !util.IsCloudRun() {
+			return handler(l.ZeroLogger.WithContext(ctx), req)
 		}
 
-		logger.Info().Msgf("ctx : %v", ctx)
-
 		if traceID := util.GetTraceIDFromMetadata(ctx); traceID != "" {
-			logger.UpdateContext(func(c zerolog.Context) zerolog.Context {
+			l.ZeroLogger.UpdateContext(func(c pkgzerolog.Context) pkgzerolog.Context {
 				return c.Str("logging.googleapis.com/trace", fmt.Sprintf("projects/%s/traces/%s", projectID, traceID))
 			})
 		}
 
-		return handler(ctx, req)
+		return handler(l.ZeroLogger.WithContext(ctx), req)
 	}
 }
 
-func ErrorHandlerInterceptor() grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		logger := clog.NewLogger(log.Ctx(ctx))
-
-		res, err := handler(ctx, req)
-		if err != nil {
-			logger.Error(err)
-			return nil, err
-		}
-
-		return res, nil
-	}
-}
-
-func InjectClientAuthInterceptor(idToken string) grpc.UnaryClientInterceptor {
+func AuthInterceptor(idToken string) grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		md := metadata.New(map[string]string{"authorization": fmt.Sprintf("Bearer %s", idToken)})
 		ctx = metadata.NewOutgoingContext(ctx, md)
 
-		return nil
+		return invoker(ctx, method, req, reply, cc, opts...)
 	}
 }
---
+++ grpc/server_test.go
@@ -0,0 +1,56 @@
+package grpc
+
+import (
+	"bytes"
+	"context"
+	"net"
+	"testing"
+
+	"github.com/ishii1648/cloud-run-sdk/logging/zerolog"
+	"github.com/ishii1648/cloud-run-sdk/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/interop"
+	pb "google.golang.org/grpc/interop/grpc_testing"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+const buffsize = 1024
+
+func TestStartServer(t *testing.T) {
+	buf := &bytes.Buffer{}
+	rootLogger := zerolog.SetLogger(buf, true, false)
+
+	expected := `{"severity":"INFO","method":"/grpc.testing.TestService/EmptyCall","message":"message"}` + "\n"
+
+	fn := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		zerolog.Ctx(ctx).Info("message")
+
+		if want, got := expected, string(buf.Bytes()); got != want {
+			t.Errorf("want %q, got %q", want, got)
+		}
+
+		return handler(ctx, req)
+	}
+
+	s := NewServer(rootLogger, "google-sample-project", fn)
+	lis := bufconn.Listen(buffsize)
+
+	pb.RegisterTestServiceServer(s.srv, interop.NewTestServer())
+
+	go s.StartServer(lis, util.SetupSignalHandler())
+
+	ctx := context.Background()
+	dial := func(context.Context, string) (net.Conn, error) {
+		return lis.Dial()
+	}
+
+	opts := []grpc.DialOption{grpc.WithInsecure(), grpc.WithContextDialer(dial)}
+	conn, err := grpc.DialContext(ctx, "bufnet", opts...)
+	if err != nil {
+		t.Fatalf("failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTestServiceClient(conn)
+	interop.DoEmptyUnaryCall(client)
+}
